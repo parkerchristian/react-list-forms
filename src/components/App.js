3b36e61ed437ffb8b6e1af6e8002a2106a652cb2
Parker Helland <hellandp@gmail.com>
2019-05-15 16:44:58 -0700

    made header footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, { PureComponent } from 'react';
+import Header from './header/Header.js';
+import Footer from './footer/Footer.js';
 
 export default class App extends PureComponent {
   render() {
     return (
       <>
+      <Header />
+      <Footer />
       </>
     );
   }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import StyleSheet from './Footer.css';
+
+export default function Footer() {
+  return (
+    <footer className={StyleSheet.Footer}>
+      <h1>Contact®</h1>
+    </footer>
+  );
+}
