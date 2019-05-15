74ef96b8a4142c8c9f1f637eba7cb7ac58edd411
Parker Helland <hellandp@gmail.com>
2019-05-15 15:41:37 -0700

    colorlist and app.js

---
+++ src/components/ColorList.js
@@ -0,0 +1,26 @@
+import React, { PureComponent } from 'react';
+import CreateColor from './CreateColor';
+import Colors from './Colors';
+
+export default class ColorList extends PureComponent {
+  state = {
+    colors: []
+  }
+
+  addColor = color => {
+    this.setState(state => {
+      return {
+        colors: [...state.colors, color]
+      };
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <CreateColor addColor={this.addColor} />
+        <Colors colors={this.state.colors} />
+      </>
+    );
+  }
+}
