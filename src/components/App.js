192940b3f692a56d5e5a92f07e03d0cff5a905d4
Parker Helland <hellandp@gmail.com>
2019-05-15 11:55:36 -0700

    testing using class

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Colors from './Colors';
 
-export default function App() {
-  const colors = [
-    { name: 'red', rgb: { red: 255, green: 0, blue: 0 } },
-    { name: 'green', rgb: { red: 0, green: 255, blue: 0 } },
-    { name: 'blue', rgb: { red: 0, green: 0, blue: 255 } }
-  ];
-  return (
-    <Colors colors={colors}/>
-  );
+export default class App extends PureComponent {
+  render() {
+    const colors = [
+      { name: 'red', rgb: { red: 255, green: 0, blue: 0 } },
+      { name: 'green', rgb: { red: 0, green: 255, blue: 0 } },
+      { name: 'blue', rgb: { red: 0, green: 0, blue: 255 } }
+    ];
+    return (
+      <Colors colors={colors}/>
+    );
+  }
 }
