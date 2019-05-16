ff6246e70064a48681a13c6dfbd908c488409f16
Parker Helland <hellandp@gmail.com>
2019-05-16 13:44:58 -0700

    app set up

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React, { PureComponent } from 'react';
+import Error from './Error';
+import Bad from './Bad';
+import Good from './Good';
 
 export default class App extends PureComponent {
   render() {
     return (
-      <></>
+      <>
+      <Error>
+        <Bad />
+      </Error>
+      <Error>
+        <Good />
+      </Error>
+      </>
     );
   }
 }
