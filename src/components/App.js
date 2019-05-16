fdfda6439947e905a9066523562a3c9563cfbc5a
Parker Helland <hellandp@gmail.com>
2019-05-16 11:19:46 -0700

    links work to update content using class

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,32 @@ import React, { PureComponent } from 'react';
 import Sidebar from './Sidebar';
 
 export default class App extends PureComponent {
+  state = {
+    page: 'home'
+  }
+
+  handler = (event, page) => {
+    console.log('***', page);
+    this.setState({ page: page });
+  }
+
   render() {
+
+    const content = {
+      home: 'YOURE HOME',
+      food: 'food nummies',
+      about: 'at about page',
+      contact: 'never call',
+    };
     return (
       <>
       <Sidebar>
-        <a href="#">HOME</a>
-        <a href="#">FOOD</a>
-        <a href="#">ABOUT</a>
-        <a href="#">CONTACT</a>
+        <a onClick={() => this.handler(null, 'home')} href="#">HOME</a>
+        <a onClick={() => this.handler(null, 'food')} href="#">FOOD</a>
+        <a onClick={() => this.handler(null, 'about')} href="#">ABOUT</a>
+        <a onClick={() => this.handler(null, 'contact')} href="#">CONTACT</a>
       </Sidebar>
-      <p>Sup</p>
+      <p>{content[this.state.page]}</p>
       </>
     );
   }
